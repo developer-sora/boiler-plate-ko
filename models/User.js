488bd596ea34c5a6089073678b416bf2b1162b76
DESKTOP-PUHRM72\sodao <sodaoov@gmail.com>
2021-08-04 19:47:29 +0900

    비밀 정보 보호

--- models/User.js
+++ models/User.js
@@ -1,37 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    name:{
-        type:String,
-        maxlength:50
-    },
-    email:{
-        type:String,
-        trim:true,
-        unique:1
-    },
-    password:{
-        type:String,
-        minlength:5
-    },
-    lastname:{
-        type:String,
-        maxlength:50
-    },
-    role:{
-        type:Number,
-        default:0
-    },
-    image:String,
-    token:{
-        type:String
-    },
-    tokenExp:{
-        type:Number
-    }
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true, // 스페이스(공백) 없애줌
+    unique: 1, // 똑같은 이메일은 쓰지 못하게
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    // 유저 등급
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
 
-})
+const User = mongoose.model("User", userSchema);
 
-const User = mongoose.model('User',userSchema);
-
-module.exports={User}
+module.exports = { User };
